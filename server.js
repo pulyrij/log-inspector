4958fa0b7c93dcb724b8ab06afb033b742676f38
Dima <pylyadima29@gmail.com>
2026-04-12 10:16:27 +0300

    log example finished, server receiving log first proto

---
+++ server.js
@@ -0,0 +1,35 @@
+import http from 'http';
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'POST' && req.url === '/logs') {
+        req.on('data', chunk => {
+            body += chunk;
+        });
+
+        req.on('end', () => {
+            try {
+                const log = JSON.parse(body);
+
+                console.log('LOG RECEIVED');
+                console.log(log);
+
+                res.writeHead(204);
+                res.end();
+            } catch (err) {
+
+                res.writeHead(400);
+                res.end("Invalid JSON");
+            }
+        });
+        
+        return;
+    }
+    res.writeHead(404);
+    res.end();
+});
+
+server.listen(PORT, () => {
+    console.log(`Logger server running on port${PORT}`);
+});
