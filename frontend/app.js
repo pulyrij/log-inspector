964870a6e3c298670e28b9e291b09fac220dddd7
Dima <pylyadima29@gmail.com>
2026-04-20 17:24:54 +0300

    scroll logic

--- frontend/app.js
+++ frontend/app.js
@@ -2,13 +2,22 @@ import store from './store.js';
 import connect from './socket.js';
 import createLogElement from './log-creator.js';
 
-connect();
+connect();  
+
+const scroller = document.getElementById('main');
 
 const logContainer = document.getElementById('log-container');
 
 function renderLog(viewModel) {
+    const shouldScroll = isAtBottom(scroller);
+
     const logElement = createLogElement(viewModel);
+
     logContainer.appendChild(logElement);
+
+    if (shouldScroll) {
+        scroller.scrollTop = scroller.scrollHeight;
+    }
 }
 
 setInterval(() => {
@@ -21,3 +30,12 @@ setInterval(() => {
 
     });
 }, 100);
+
+function isAtBottom(el, threshold = 4) {
+    return (
+        el.scrollHeight
+    - el.scrollTop
+    - el.clientHeight
+    ) <= threshold;
+    
+}
