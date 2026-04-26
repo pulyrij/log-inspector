0e32e7ba92369e3884a6f749c83eecc074fe80bf
Dima <pylyadima29@gmail.com>
2026-04-26 11:15:18 +0300

    engine.dispatch

--- back/engine.js
+++ back/engine.js
@@ -1,120 +1,14 @@
-const engine1 = {
-    receive: () => {},
-    dispatch: () => {}
-}
-
-export default engine;
-
-const LOG_TYPES = ['alert', 'error', 'data'];
-
-const store = {
-    logs: [],
-
-    add(logRecord) {
-        this.logs.push(logRecord);
-    }
-};
-
-function transformRawLog(log) {
-    const id = `${log.sessionId}${String(log.logId).padStart(4, '0')}`;
-    return {
-        id,
-        timestamp: log.timestamp,
-        sessionId: log.sessionId,
-        pid:  log.pid ?? 0,
-        hostname : log.hostname ?? 'host',
-        type: log.type,
-        service: log.service ?? 'application',
-        module: log.module ?? '/',
-        level: log.metadata.level ?? 'info',
-        message: log.metadata.message
-    }
-}
-
-export default function logPipeline(log){
-    const result = validateRawLog(log);
-    if (!result.isValid) {
-        return {
-            statusCode: 400,
-            end: (JSON.stringify({
-                ok: false,
-                errors: result.errors
-            }))
-        }
-    }
-
-    console.log(log)
-    console.log('2')
-    const logRecord = transformRawLog(log);
-    console.log(logRecord);
-
-    console.log(logRecord)
-    console.log('3')
-    store.add(logRecord);
-    const viewModel = createLogViewModel(logRecord);
-    console.log(viewModel)
-    console.log('4')
-    broadcast(viewModel);
-    return {
-        statusCode: 200,
-        end: (JSON.stringify({
-            ok: true,
-            id: logRecord.id
-        }))
-    }
-}
-
-function getLogs() {
-    return store.logs;
-}
-
-function createLogViewModel(log) {
-    const datetime = new Date(log.timestamp)
-        .toLocaleString('sv-SE')
-        .replace(' ', 'T')
-        .slice(0, 16);
-    const [date, time] = datetime.split('T');
-
-    return {
-        id: log.id,
-        type: log.type,
-        level: log.level,
-        datetime: datetime,
-
-        header: {
-            time: time,
-            message: log.message,
-            service: log.service
-        },
-
-        layouts: [
-            {
-                name: 'meta',
-                date: date,
-                module: log.module,
-                pid: log.pid,
-                hostname: log.hostname,
-            }
-        ],
-
-        ui: {
-            expandedLayouts: {
-                meta: false
-            }
-        }
-    }
-}
-
 const LOG_TYPES = ['alert', 'error', 'data'];
 
 class Engine{
     #store;
+    #subscribers;
     constructor() {
-        this.store = [];
-
+        this.#store = [];
+        this.#subscribers = new Set();
     }
-    receive(log) {
-        const { isValid, errors } = this.#validateRawLog(log);
+    receive(rawLog) {
+        const { isValid, errors } = this.#validateRawLog(rawLog);
 
         if (!isValid) {
             return {
@@ -123,13 +17,21 @@ class Engine{
             }
         }
 
+        const logRecord = this.#transformRawLog(rawLog);
+        this.#store.push(logRecord);
+
+        const viewModel = this.#createLogViewModel(logRecord);
+        this.#subscribers.forEach(cb => cb.viewModel);
+
+
         return {
             statusCode: 200,
             body: { ok: true }
         }
     }
-    dispatch() {
-
+    dispatch(callback) {
+        this.#subscribers.add(callback);
+        return () => this.#subscribers.delete(callback);
     }
     #validateRawLog(log) {
         if (!log || typeof log !== 'object') {
@@ -157,6 +59,57 @@ class Engine{
         return {
             isValid: errors.length === 0,
             errors
+        }
+    }
+    #transformRawLog(log) {
+        const id = `${log.sessionId}${String(log.logId).padStart(4, '0')}`;
+        return {
+            id,
+            timestamp: log.timestamp,
+            sessionId: log.sessionId,
+            pid:  log.pid ?? 0,
+            hostname : log.hostname ?? 'host',
+            type: log.type,
+            service: log.service ?? 'application',
+            module: log.module ?? '/',
+            level: log.metadata.level ?? 'info',
+            message: log.metadata.message
+        }
+    }
+    #createLogViewModel(log) {
+        const datetime = new Date(log.timestamp)
+            .toLocaleString('sv-SE')
+            .replace(' ', 'T')
+            .slice(0, 16);
+        const [date, time] = datetime.split('T');
+
+        return {
+            id: log.id,
+            type: log.type,
+            level: log.level,
+            datetime: datetime,
+
+            header: {
+                time: time,
+                message: log.message,
+                service: log.service
+            },
+
+            layouts: [
+                {
+                    name: 'meta',
+                    date: date,
+                    module: log.module,
+                    pid: log.pid,
+                    hostname: log.hostname,
+                }
+            ],
+
+            ui: {
+                expandedLayouts: {
+                    meta: false
+                }
+            }
         }
     }
 }
